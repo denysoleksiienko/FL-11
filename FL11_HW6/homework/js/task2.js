50425787b0d648493bd7137104e30fad867a2b70
Denis Alekseenko <42835486+denysoleksiienko@users.noreply.github.com>
2019-07-07 12:30:00 +0300

    Update task2.js

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,6 +1,6 @@
 let a = parseFloat(prompt('Please, enter the first length of the triangle side (A)', '')),
-b = parseFloat(prompt('Please, enter the second length of the triangle side (B)', '')),
-c = parseFloat(prompt('Please, enter the third length of the triangle side (C)', ''));
+    b = parseFloat(prompt('Please, enter the second length of the triangle side (B)', '')),
+    c = parseFloat(prompt('Please, enter the third length of the triangle side (C)', ''));
 
 if (a + b > c && b + c > a && a+ c > b) {
 	if (a === b && b === c) {
