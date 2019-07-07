3d3b314c2d6815af95907d0a543c184ff16b4131
Denis Alekseenko <42835486+denysoleksiienko@users.noreply.github.com>
2019-07-07 20:39:47 +0300

    Update task2.js

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -2,7 +2,7 @@ let a = parseFloat(prompt('Please, enter the first length of the triangle side (
     b = parseFloat(prompt('Please, enter the second length of the triangle side (B)', '')),
     c = parseFloat(prompt('Please, enter the third length of the triangle side (C)', ''));
 
-if (a + b > c && b + c > a && a+ c > b) {
+if (a + b > c && b + c > a && a + c > b) {
     if (a === b && b === c) {
         console.log('Eequivalent triangle');
     } else if (a === b || b === c || c === a) {
