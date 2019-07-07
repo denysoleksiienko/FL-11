5116308c6e090f4c5d3feeb01c4a2e34547e207a
denysoleksiienko <42835486+denysoleksiienko@users.noreply.github.com>
2019-07-07 12:01:38 +0300

    Update task1.js
    some correct error and optimizing code

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,14 +1,14 @@
-let a1 = parseFloat(prompt('Please, enter your x coordination of "A1" point', ''));
-let a2 = parseFloat(prompt('Please, enter your y coordination of "A2" point', ''));
-let b1 = parseFloat(prompt('Please, enter your x coordination of "B1" point', ''));
-let b2 = parseFloat(prompt('Please, enter your y coordination of "B2" point', ''));
-let c1 = parseFloat(prompt('Please, enter your x coordination of "C1" point', ''));
-let c2 = parseFloat(prompt('Please, enter your y coordination of "C2" point', ''));
+let a1 = parseFloat(prompt('Please, enter your x coordination of "A1" point', '')),
+    a2 = parseFloat(prompt('Please, enter your y coordination of "A2" point', '')),
+    b1 = parseFloat(prompt('Please, enter your x coordination of "B1" point', '')),
+    b2 = parseFloat(prompt('Please, enter your y coordination of "B2" point', '')),
+    c1 = parseFloat(prompt('Please, enter your x coordination of "C1" point', '')),
+    c2 = parseFloat(prompt('Please, enter your y coordination of "C2" point', ''));
 
-let half = 2;
+const half = 2;
 
-let x = (a1 + b1) / half;
-let y = (a2 + a2) / half;
+let x = (a1 + b1) / half,
+    y = (a2 + b2) / half;
 
 if (c1 === x && c2 === y) {
     console.log(true);
