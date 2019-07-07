5c8d4bbbdcd340c1e43f5654e25475ad4d4c52ba
Denis Alekseenko <42835486+denysoleksiienko@users.noreply.github.com>
2019-07-07 12:35:48 +0300

    Update task2.js

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -3,13 +3,13 @@ let a = parseFloat(prompt('Please, enter the first length of the triangle side (
     c = parseFloat(prompt('Please, enter the third length of the triangle side (C)', ''));
 
 if (a + b > c && b + c > a && a+ c > b) {
-	if (a === b && b === c) {
-		console.log('Eequivalent triangle');
-	} else if (a === b || b === c || c === a) {
-		console.log('Isosceles triangle');
-	} else {
-		console.log('Normal triangleconst');
-	}
+    if (a === b && b === c) {
+        console.log('Eequivalent triangle');
+    } else if (a === b || b === c || c === a) {
+        console.log('Isosceles triangle');
+    } else {
+        console.log('Normal triangleconst');
+    }
 } else {
     console.log('Triangle doesn’t exist');
 }
