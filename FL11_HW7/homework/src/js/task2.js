37ee2a2ef0240edad9f2a30b9564b195dba3a0c8
denysoleksiienko <42835486+denysoleksiienko@users.noreply.github.com>
2019-07-09 18:44:52 +0300

    task 2 done

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,44 @@
-// Your code goes here
+let startGame = confirm('Do you want to play a game?');
+let multiplierPrize = 2,
+    attempts = 3,
+    plusRange = 4,
+    defaultAttempts = 3,
+    defaultPrize = 100,
+    defaultRange = 8,
+    newPrize = defaultPrize,
+    userPrize = 0;
+
+if (!startGame) {
+    alert('You did not become a billionaire, but can.');
+} else {
+    do {
+        for (defaultAttempts = attempts; defaultAttempts > 0; defaultAttempts--) {
+            let random = Math.round(Math.random() * defaultRange);
+            let user = prompt(
+`Enter the number between 0 and ${defaultRange}
+Attempts left: ${defaultAttempts}
+Total prize: ${userPrize}$
+Possible prize on current attempt: ${defaultPrize}$`, '');
+            if (user === random) {
+                userPrize += newPrize;
+                newPrize *= multiplierPrize;
+                random = Math.round(Math.random() * defaultRange);
+                startGame = confirm(
+`Congratulation, you won! Your prize is: ${userPrize}$. Do you want to continue?`);
+                if (!startGame) {
+                    break;
+                } else {
+                    defaultRange += plusRange;
+                    defaultAttempts = attempts;
+                    defaultPrize *= multiplierPrize;
+                    newPrize = defaultPrize;
+                }
+            } else {
+                newPrize /= multiplierPrize;
+            }
+        }
+        alert(
+`Thank you for your participation. Your prize is: ${userPrize}$`);
+        startGame = confirm('Do you want to play again?');
+    } while (startGame);
+}
